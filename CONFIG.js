ef531dd31c5c45ea956283fb5f13cef50d9d9bcc
thanktiam <tamtrinhdn@gmail.com>
2025-01-14 00:09:58 +0700

    i like u

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn cậu.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    // "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh cậu. </br> Tớ biết tớ không được xênh, ovtk, hướng lung tung cũng chẳng lắm tài nhưng tớ có một trái tim cute, tớ nhát nên gặp cậu là tôi chỉ biết né thôi do cậu nhiều người thích quá. </br> Tối nào đi chơi cùng tớ được ko?.</br> Love you <3",
 };
